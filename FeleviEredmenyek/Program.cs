8f210ed39566b1c24d5ed53c77b1a5f5aa0114a2
beec <beec1000.minecraft@gmail.com>
2023-11-07 13:16:01 +0100

    Add project files.

---
+++ FeleviEredmenyek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FeleviEredmenyek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jegyek = new List<Tanulo>();
+
+            using var sr = new StreamReader(@"..\..\..\src\Adatok.txt");
+            {
+                _ = sr.ReadLine();
+
+            }
+
+        }
+    }
+}
---
+++ FeleviEredmenyek/Tanulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace FeleviEredmenyek
+{
+    class Tanulo
+    {
+        public string Neve { get; set; }
+        public int Azonosito { get; set; }
+        public Dictionary<string, int> Jegyek { get; set; }
+
+        public Tanulo(string sor)
+        {
+            var v = sor.Split(' ');
+            this.Neve = v[0];
+            this.Azonosito = int.Parse(v[1]);
+            for (int i = 2; i < v.Length; i += 2)
+            {
+                this.Jegyek[v[i]] = int.Parse(v[i + 1]);
+            }
+        }
+
+        static void Atlag(int j)
+        {
+            var x = 
+        }
+
+    }
+}
