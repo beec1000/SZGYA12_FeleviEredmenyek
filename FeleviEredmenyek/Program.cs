cb6e32edd38142544faabf83b32d9af4247c6155
beec1000 <beec1000.minecraft@gmail.com>
2023-11-19 21:35:19 +0100

    u

--- FeleviEredmenyek/Program.cs
+++ FeleviEredmenyek/Program.cs
@@ -1,27 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace FeleviEredmenyek
 {
     class Program
     {
+        static double F1OsszAt(List<Tanulo> a)
+        {
+            var x = a.Average(d => d.Atlag);
+            return x;
+        }
+        static Dictionary<string, double> F1TantargyAt(List<Tanulo> a)
+        {
+            Dictionary<string, double> atlagok = new Dictionary<string, double>();
+            foreach (var i in a.First().Jegyek.Keys)
+            {
+                atlagok[i] = a.Average(d => d.Jegyek[i]);
+            }
+            return atlagok;
+        }
+
         static void Main(string[] args)
         { 
             var adatok = new List<Tanulo>();
 
             using var sr = new StreamReader(@"..\..\..\src\Adatok.txt");
-
-            string tantargy = sr.ReadLine();
-            List<string> tantargyNevek = new List<string>(tantargy.Split('\t'));
-            for (int i = 0; i < adatok.Count; i++)
             {
-                adatok.Add(new Tanulo(sr.ReadLine(), tantargyNevek));
-            }
+                string tantargy = sr.ReadLine();
+                List<string> tantargyNevek = new List<string>(tantargy.Split('\t'));
+                for (int i = 0; !sr.EndOfStream; i++)
+                {
+                    if (i % 2 ==  0)
+                    {   
+                        adatok.Add(new Tanulo(sr.ReadLine(), tantargyNevek));
+                    }
+                }
 
-            foreach (var i in adatok)
-            {
-                Console.WriteLine(i.ToString());
+                Console.WriteLine($"1. feladat \nAz osztály átlaga: {F1OsszAt(adatok)}");
+                foreach (var i in F1TantargyAt(adatok))
+                {
+                    Console.WriteLine($"{i.Key}: {i.Value}");
+                }
+
             }
 
         }
--- FeleviEredmenyek/Tanulo.cs
+++ FeleviEredmenyek/Tanulo.cs
@@ -9,34 +9,25 @@ namespace FeleviEredmenyek
     {
         public string Neve { get; set; }
         public long Azonosito { get; set; }
-        public Dictionary<string, List<int>> Jegyek { get; set; }
-
-        //public double Atlag => 
+        public Dictionary<string, int> Jegyek { get; set; }
+        public double Atlag => this.Jegyek.Values.Average();
 
         public Tanulo(string sor, List<string> tantargyak)
         {
-            var v = sor.Split(' ');
+            var v = sor.Split("\t");
             this.Neve = v[0];
             this.Azonosito = long.Parse(v[1]);
-            this.Jegyek = new Dictionary<string, List<int>>();
-            for (int i = 2; i < v.Length; i += 2)
+            this.Jegyek = new Dictionary<string, int>();
+            for (int i = 2; i < v.Length; i++)
             {
-                string tantargy = tantargyak[i - 2];
                 int jegy = int.Parse(v[i]);
-
-                if (!Jegyek.ContainsKey(tantargy))
-                {
-                    Jegyek[tantargy] = new List<int>();
-                }
-
-                Jegyek[tantargy].Add(jegy);
+                this.Jegyek[tantargyak[i]] = jegy;
             }
         }
 
         public override string ToString()
         {
-            return $"{this.Neve}, {this.Azonosito}, {this.Jegyek}";
+            return $"{this.Neve}, {this.Azonosito}, {string.Join("; ", this.Jegyek)}";
         }
-
     }
 }
